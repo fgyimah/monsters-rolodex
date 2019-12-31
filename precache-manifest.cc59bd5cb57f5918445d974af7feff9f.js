976d575c7be5fdd39c5e45ee3d4a2a6b9c1e2616
Francis Gyimah <fgyimah883@gmail.com>
2019-12-31 03:06:42 +0000

    Updates

---
+++ precache-manifest.cc59bd5cb57f5918445d974af7feff9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f627a829f1f11c90cdda395bf2dfb6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "66432c782b2d2e71a841",
+    "url": "/monsters-rolodex/static/css/main.94825b71.chunk.css"
+  },
+  {
+    "revision": "105422028b6763bd0844",
+    "url": "/monsters-rolodex/static/js/2.f35d3ce1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.f35d3ce1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "66432c782b2d2e71a841",
+    "url": "/monsters-rolodex/static/js/main.99c23eea.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
